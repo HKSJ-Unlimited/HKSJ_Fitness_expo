bc2f5f7574c2118485fa22c8e6a05427cbd72525
rohit singh negi <rocknegi53@gmail.com>
2024-11-29 17:17:13 +0100

    setup nativewind install icons create theme swtich component remove unused files code cleanmup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,4 @@
-import { Pressable, StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { Button, Text, StyleSheet, View, Pressable } from "react-native";
 import {
   GoogleSignin,
   GoogleSigninButton,
@@ -54,17 +51,9 @@ export default function TabOneScreen() {
   };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <GoogleSigninButton onPress={signIn} />
-      <Pressable onPress={signOut}>
-        <Text>Signout</Text>
+      <Pressable className=" p-4 max-w-[100px]">
+        <Text className="text-foreground">Sign In</Text>
       </Pressable>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }

908300b9c6a450d671f480d2b7250d8d3112c654
rohit singh negi <rocknegi53@gmail.com>
2025-02-22 12:35:29 +0100

    feat: add migration scripts for new diary_table structure and totalCalories column in meals_table

---
+++ drizzle/0011_bizarre_raza.sql
@@ -0,0 +1,15 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_diary_table` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`userId` integer NOT NULL,
+	`date` text DEFAULT '2025-02-22T00:49:11.732Z' NOT NULL,
+	`mealId` integer NOT NULL,
+	FOREIGN KEY (`userId`) REFERENCES `users_table`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`mealId`) REFERENCES `meals_table`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_diary_table`("id", "userId", "date", "mealId") SELECT "id", "userId", "date", "mealId" FROM `diary_table`;--> statement-breakpoint
+DROP TABLE `diary_table`;--> statement-breakpoint
+ALTER TABLE `__new_diary_table` RENAME TO `diary_table`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+ALTER TABLE `meals_table` ADD `totalCalories` integer NOT NULL;

6bf47d1671ebfdebaa962a176a77f19abf0c0fe6
rohit singh negi <rocknegi53@gmail.com>
2025-02-22 12:35:56 +0100

    feat: add totalCalories insertion for meal types in AuthLayout component

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -2,10 +2,11 @@ import { Stack } from "expo-router";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import { useEffect } from "react";
-import { goalsTable, usersTable } from "@/db/schema";
+import { goalsTable, totalCalories, usersTable } from "@/db/schema";
 import { useSQLiteContext } from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { getApp } from "@react-native-firebase/app";
+import { mealType } from "@/Types/SharedTypes";
 
 export default function AuthLayout() {
   const db = useSQLiteContext();
@@ -24,6 +25,19 @@ export default function AuthLayout() {
         calories: 2000,
         weight: 69,
         userId: user[0].id,
+      });
+      await drizzleDb.insert(totalCalories).values({
+        type: mealType.breakfast,
+      });
+
+      await drizzleDb.insert(totalCalories).values({
+        type: mealType.lunch,
+      });
+      await drizzleDb.insert(totalCalories).values({
+        type: mealType.dinner,
+      });
+      await drizzleDb.insert(totalCalories).values({
+        type: mealType.snack,
       });
     }
   }
